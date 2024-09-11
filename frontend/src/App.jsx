dbf77702ac20b4e207273ec397afc683b7622eaf
harrisonr98 <harrisonr98@yahoo.com>
2024-09-11 12:29:48 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className='flex flex-col items-center justify-center h-screen gap-16'>
-      <h1 className='text-center text-8xl'>Hello, World!</h1>
+      <h1 className='text-center text-8xl'>Hellooooo, World!</h1>
       <p className='text-2xl'>This uses React, Vite, Tailwind CSS, and Shadcn (JS version)</p>
       <div className='mx-auto w-fit'>
         <Button onClick={() => setCount((count) => count + 1)}>Count is {count}</Button>
