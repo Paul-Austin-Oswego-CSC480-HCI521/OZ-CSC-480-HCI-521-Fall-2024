260eea0e91c0c24d7801f026ed46b1e349e00bf9
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-09-25 12:44:51 -0400

    Update styling to match demo wireframe

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
 export function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -21,12 +21,57 @@ export function Login() {
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <Card className="w-[350px] h-[525px]">
+            <Card className="w-[350px]">
+                <div className='border-b flex items-center justify-center gap-2 py-2'>
+                    <Avatar className="border">
+                        <AvatarImage src=""/>
+                        <AvatarFallback>L</AvatarFallback>
+                    </Avatar>
+                    <p className='text-xl font-bold'>Brand</p>
+                </div>
                 <CardHeader>
-                    <CardTitle className="flex items-center justify-center">Login</CardTitle>
+                    <CardTitle className="flex items-center justify-center">Log in to continue</CardTitle>
                 </CardHeader>
-                <CardContent>
-                    <form onSubmit={handleSubmit}>
+                <CardContent className='flex flex-col px-8'>
+                <Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlnsXlink="http://www.w3.org/1999/xlink" style={{width: "20px", height: "20px", margin: "0 8px"}} >
+        <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+        <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+        <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+        <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+        <path fill="none" d="M0 0h48v48H0z"></path>
+      </svg>
+                        
+                        Continue with Google
+                    </Button>
+                    <br/>
+                    <Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
+                        <svg className="mr-2 h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab"
+                             data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
+                        </svg>
+                        Continue with TBD
+                    </Button>
+                    <br/>
+                    <Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
+                        <svg className="mr-2 h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab"
+                             data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
+                        </svg>
+                        Continue with TBD
+                    </Button>
+                    
+                </CardContent>
+                <CardFooter className="flex flex-col px-8">
+                    <div className="relative w-full mb-6">
+                        <div className="absolute inset-0 flex items-center">
+                            <span className="w-full border-t"/>
+                        </div>
+                        <div className="relative flex justify-center text-xs uppercase">
+                            <span className="bg-background px-2 text-muted-foreground">
+                                Or continue with
+                            </span>
+                        </div>
+                    </div>
+                    <form onSubmit={handleSubmit} className='w-full'>
                         <div className="grid w-full items-center gap-4">
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="email">Email</Label>
@@ -54,40 +99,6 @@ export function Login() {
                             Log in
                         </Button>
                     </form>
-                </CardContent>
-                <CardFooter className="flex flex-col">
-                    <div className="relative w-full mb-6">
-                        <div className="absolute inset-0 flex items-center">
-                            <span className="w-full border-t"/>
-                        </div>
-                        <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">
-                Or continue with
-              </span>
-                        </div>
-                    </div>
-                    <Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
-                        <svg className="mr-2 h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab"
-                             data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-                            <path fill="currentColor"
-                                  d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
-                        </svg>
-                        Login with Google
-                    </Button>
-                    <br/>
-                    <Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
-                        <svg className="mr-2 h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab"
-                             data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-                        </svg>
-                        Placeholder
-                    </Button>
-                    <br/>
-                    <Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
-                        <svg className="mr-2 h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab"
-                             data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-                        </svg>
-                        Placeholder
-                    </Button>
                 </CardFooter>
             </Card>
         </div>
