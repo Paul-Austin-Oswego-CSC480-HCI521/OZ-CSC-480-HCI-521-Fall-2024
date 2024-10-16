8c04acf97d19d27c315e5084ee3408c964cc017b
Daniel Maslowski <dmaslows@oswego.edu>
2024-10-16 14:27:44 -0400

    QA Update
    Added dependencies and plugins for JUNIT, and added test code with the appropriate file structure.

---
+++ backend/database-service/target/liberty/wlp/lib/platform/checksums/com.ibm.websphere.appserver.logging-1.0.cs
@@ -0,0 +1,7 @@
+#Mon Sep 23 18:38:46 UTC 2024
+lib/platform/defaultLogging-1.0.mf=4464aa9772a9431194bdd1e15e7e6ebc
+dev/spi/ibm/javadoc/com.ibm.websphere.appserver.spi.logging_1.1-javadoc.zip=3ca0c288c3d9600884899087d9e8d072
+lib/com.ibm.ws.collector.manager_1.0.94.jar=e578df328a6c28e791856011ab366405
+lib/com.ibm.ws.logging.osgi_1.0.94.jar=42c0f687b42abe525935ae2ffed6ec0b
+dev/spi/ibm/com.ibm.websphere.appserver.spi.logging_1.1.94.jar=75ef8ed20c96915f97557430af3c92ed
+lib/com.ibm.ws.logging_1.0.94.jar=cadd5e5ea41e70ccc16e3dc326340cae
