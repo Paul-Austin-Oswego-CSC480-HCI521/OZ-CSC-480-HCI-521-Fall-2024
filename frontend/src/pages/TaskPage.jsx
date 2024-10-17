7b5b84257ec44f83fa06680486adf230abc95760
Abdullie <adiallo7@oswego.edu>
2024-10-17 16:15:49 -0400

    sidebar

--- frontend/src/pages/TaskPage.jsx
+++ frontend/src/pages/TaskPage.jsx
@@ -2,7 +2,8 @@ import React, {useState} from "react";
 import {Input} from "@/components/ui/input.jsx";
 import {Button} from "@/components/ui/button.jsx";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table.jsx";
-import {ArchiveIcon, CheckIcon} from "@radix-ui/react-icons";
+import { Sidebar } from "@/components/ui/sidebar";
+import { Label } from "@radix-ui/react-label";
 
 
 export function TaskPage() {
@@ -30,7 +31,8 @@ export function TaskPage() {
 
     return (
         <>
-            <div className='flex flex-col items-center justify-center h-screen gap-16'>
+        <div className = 'flex h-screen'>
+            <div className='flex flex-col items-center justify-center h-screen gap-16 flex-1'>
                 <h1 className='text-center text-8xl'>Hello, World!</h1>
                 <p className='text-2xl'>This uses React, Vite, Tailwind CSS, and Shadcn (JS version)</p>
 
@@ -84,6 +86,64 @@ export function TaskPage() {
                     </Table>
                 </div>
             </div>
+            <Sidebar title={"Test Title"}>
+            <select
+                    className="w-full p-2 border bg-white rounded focus:outline-none focus:ring-1 focus:ring-black mb-4"
+                >
+                    <option value="option1">Project 1</option>
+                    <option value="option2">Project 2</option>
+                    <option value="option3">Project 3</option>
+                    <option value="option1">No Project</option>
+                </select>
+
+
+                <input
+                type="date"
+                className="flex h-10 w-full rounded-md border border-neutral-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300 mb-4"
+                >
+                </input>
+
+
+                <select
+                    className="w-full p-2 border bg-white rounded focus:outline-none focus:ring-1 focus:ring-black mb-4"
+                >
+                    <option value="option1">Repeats Never</option>
+                    <option value="option2">To be determined</option>
+                    <option value="option3">To be determined</option>
+                    <option value="option1">To be determined</option>
+                </select>
+
+
+                <Input
+                className ='mb-4'
+                placeholder="Enter tags separated by commas">
+                </Input>
+
+
+                <select
+                    className="w-full p-2 border bg-white rounded focus:outline-none focus:ring-1 focus:ring-black mb-4"
+                >
+                    <option value="option1">Low</option>
+                    <option value="option2">Medium</option>
+                    <option value="option3">High</option>
+                    <option value="option1">No Priority</option>
+                </select>
+
+
+                <Label htmlFor="password"><b>Task Description</b></Label>
+               <textarea
+               placeholder="Describe your task here..."
+               spellCheck ='false'
+               style={{ resize: 'none' }}
+               className="flex w-full h-60 rounded-md border border-neutral-200 focus:outline-none focus:ring-1 focus:ring-black bg-white px-3 py-2 ring-offset-white file:border-0 file:bg-transparent file:font-light placeholder:text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 mb-4"
+               >
+               </textarea>
+               <div className="flex justify-end mb-4">
+               <Button variant="default2">Save Changes</Button>
+               </div>
+ 
+            </Sidebar>
+         </div>
         </>
     )
 }
