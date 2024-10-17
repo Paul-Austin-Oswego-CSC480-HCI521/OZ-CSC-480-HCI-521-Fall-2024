dfbc79c4cb9f68353383ac42b83f4203cecdd26a
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-10-16 21:20:32 -0400

    Apply page layout, left side bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,13 +15,22 @@ import * as PropTypes from "prop-types";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {TaskPage} from "@/src/pages/TaskPage.jsx";
 import { Register } from './pages/Registration'
+import PageLayout from './pages/Layout'
+import CompletedPage from './pages/CompletedPage'
+import RecentlyDeleted from './pages/RecentlyDeleted'
+import SharedWithMe from './pages/SharedWithMe'
 
 export default function Component() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<TaskPage />}/>
+                    <Route path="/" element={<PageLayout />}>
+                        <Route index element={<TaskPage />}/>
+                        <Route path="completed" element={<CompletedPage/>}/>
+                        <Route path="deleted" element={<RecentlyDeleted/>}/>
+                        <Route path="shared" element={<SharedWithMe/>}/>
+                    </Route>
                     <Route path="/Login" element={<Login/>}/>
                     <Route path = "/Registration" element = {<Register/>}/>
                 </Routes>
