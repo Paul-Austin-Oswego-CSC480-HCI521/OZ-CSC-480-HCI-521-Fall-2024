448699fa1236afb24947e7746fe3afc5007ee23b
piesarentsquare <jjhh12@outlook.com>
2024-10-24 21:31:16 -0400

    make sure features get installed when running start

--- start_website.sh
+++ start_website.sh
@@ -3,12 +3,12 @@
 # Starting authorization service
 echo "Starting authorization service..."
 cd backend/auth-service || exit
-mvn liberty:start
+mvn liberty:run &
 
 # Starting database service
 echo "Starting database service..."
 cd ../database-service || exit
-mvn liberty:start
+mvn liberty:run &
 
 # Installing dependencies and starting frontend server
 echo "Installing dependencies... Starting frontend server..."
