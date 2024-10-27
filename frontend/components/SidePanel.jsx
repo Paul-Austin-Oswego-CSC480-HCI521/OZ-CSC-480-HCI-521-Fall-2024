477c825a61416c048bb249461acb6374ff741493
s-Aura-v <slamich2@oswego.edu>
2024-10-27 16:28:49 -0400

    Updated Task Creation
    1. Added redirect as well

--- frontend/components/SidePanel.jsx
+++ frontend/components/SidePanel.jsx
@@ -1,17 +1,19 @@
 // Main left-side menu/nav panel
 import NavButton from "./NavButton.jsx"
 import ProjectAccordion from "./ProjectAccordion.jsx"
-import { LogOutIcon } from "lucide-react"
+import {LogOutIcon} from "lucide-react"
+
 export function SidePanel() {
     return (
-        <header className= " bg-[#CBD5E1] min-w-[222px] h-screen " >
-            <div className="h-36 flex justify-center items-end pb-10 ">
+        <header className=" bg-[#CBD5E1] min-w-[222px] h-screen ">
+            <div className="h-36 flex justify-center items-end pb-10 "
+                 onClick={() => window.location.href = 'http://localhost:2080/'}>
                 <div className="flex items-center gap-2">
                     <div className="bg-black w-12 h-12 rounded-full"></div>
                     <h1 className="text-2xl font-bold">Checkmate</h1>
                 </div>
             </div>
-            
+
             <nav className="">
                 <ul className="flex flex-col">
                     <li className="flex flex-col">
@@ -35,14 +37,14 @@ export function SidePanel() {
             <nav className="">
                 <ul className="flex flex-col ">
                     <li className="flex flex-col fixed bottom-10 min-w-56 ">
-                        <NavButton href="/Login" >Log out 
-                        <LogOutIcon className="resize scale-50 translate-x-24"></LogOutIcon>
+                        <NavButton href="/Login">Log out
+                            <LogOutIcon className="resize scale-50 translate-x-24"></LogOutIcon>
                         </NavButton>
-                        
+
                     </li>
                 </ul>
             </nav>
-            
+
         </header>
     )
 }
--- frontend/components/ui/sidebar.jsx
+++ frontend/components/ui/sidebar.jsx
@@ -1,61 +1,62 @@
-import React, { useState } from 'react';
-import { Pencil1Icon } from '@radix-ui/react-icons';
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import { Button } from './button';
-import { CheckIcon } from '@radix-ui/react-icons';
+// Sidebar.js
+import React, {useEffect, useState} from 'react';
+import { Pencil1Icon, CheckIcon } from '@radix-ui/react-icons';
 
-
-export function Sidebar({ title, setTitleInParent, children }) {
-     const [is_editing, edit_mode] = useState(false);
+export function Sidebar({ title, setTitleInParent, editMode, isEditMode, children }) {
     const [newTitle, set_title] = useState(title);
 
+    useEffect(() => {
+        set_title(title);
+    }, [title]);
+
+
     const pencil_click = () => {
-        edit_mode(true);
+        set_title("");
+        isEditMode(true);
     };
 
     const handle_save = () => {
-        if (newTitle.trim() === ""){
-            set_title("Empty")
-        }else{
-            set_title(newTitle)
+        if (newTitle.trim() === "") {
+            set_title("");
+        } else {
+            set_title(newTitle);
         }
-        setTitleInParent(newTitle)
-        edit_mode(false);
+        setTitleInParent(newTitle);
+        isEditMode(false);
     };
 
     return (
-       
         <div
             style={{
                 backgroundColor: '#F1F6F9',
-                height: '100vh', // full page height
-                width: '250px',  // sidebar width
-                position: 'fixed', // fixes the sidebar to the page
-                right: 0, // aligns sidebar to the rightmost part of the page
-                top: 0, // start from top of the page
+                height: '100vh',
+                width: '250px',
+                position: 'fixed',
+                right: 0,
+                top: 0,
             }}
             className="p-4 shadow-lg"
-           
         >
-             {is_editing ? (
+            {editMode ? (
                 <div className="flex items-center justify-between mb-4 w-full">
                     <input
                         type="text"
                         value={newTitle}
                         onChange={(e) => set_title(e.target.value)}
+                        autoFocus={true}
                         className="w-3/4 rounded-md border border-neutral-200 p-2 focus:outline-none focus:ring-1 focus:ring-black focus:bg-white hover:bg-gray-300 transition-colors duration-300 mt-0.5 "
                     />
                     <button onClick={handle_save}>
                         <CheckIcon className="h-5 w-5 mt-5 hover:text-green-500 transition-colors duration-200" />
                     </button>
                 </div>
-            ) :(
-            <div className='flex items-center justify-between'>
-                <h1 className="text-xl font-semibold mt-4 mb-4">{newTitle}</h1>
-                <button onClick={pencil_click} className="focus:outline-none mt-2">
-                    <Pencil1Icon className="h-4 w-4 text-gray-500" />
-                </button>
-            </div>
+            ) : (
+                <div className='flex items-center justify-between'>
+                    <h1 className="text-xl font-semibold mt-4 mb-4">{newTitle}</h1>
+                    <button onClick={pencil_click} className="focus:outline-none mt-2">
+                        <Pencil1Icon className="h-4 w-4 text-gray-500" />
+                    </button>
+                </div>
             )}
             <div>
                 {children}
