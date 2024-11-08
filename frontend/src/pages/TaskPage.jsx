d22de6698c4632f841221274c93ce3af4157fa58
harrisonr98 <harrisonr98@yahoo.com>
2024-11-08 18:29:03 -0500

    add upcoming and completed tabs

--- frontend/src/pages/TaskPage.jsx
+++ frontend/src/pages/TaskPage.jsx
@@ -1,18 +1,18 @@
-import React, {useEffect, useState} from "react";
-import {Input} from "@/components/ui/input.jsx";
-import {Button} from "@/components/ui/button.jsx";
-import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table.jsx";
-import {Sidebar} from "@/components/ui/sidebar";
-import {Label} from "@radix-ui/react-label";
-import {Trash2} from "lucide-react";
-import {ArchiveIcon, CaretSortIcon, CheckIcon} from "@radix-ui/react-icons";
-import {Checkbox} from "@/components/ui/checkbox.jsx";
-import {Dialog} from "@radix-ui/react-dialog";
-import {DialogDemo} from "@/components/Dialog.jsx";
-import {AccordionContent} from "@/components/ui/accordion.jsx";
+import React, { useEffect, useState } from "react";
+import { Input } from "@/components/ui/input.jsx";
+import { Button } from "@/components/ui/button.jsx";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table.jsx";
+import { Sidebar } from "@/components/ui/sidebar";
+import { Label } from "@radix-ui/react-label";
+import { Trash2 } from "lucide-react";
+import { ArchiveIcon, CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
+import { Checkbox } from "@/components/ui/checkbox.jsx";
+import { Dialog } from "@radix-ui/react-dialog";
+import { DialogDemo } from "@/components/Dialog.jsx";
+import { AccordionContent } from "@/components/ui/accordion.jsx";
 import NavButton from "@/components/NavButton.jsx";
 
-import {TaskTable} from '@/components/TaskTable'
+import { TaskTable } from '@/components/TaskTable'
 import { taskColumns } from "@/components/TaskColumns";
 
 // Initial tasks for test data
@@ -44,10 +44,16 @@ const initialProjects = [
 export function TaskPage() {
     const [tasks, setTasks] = useState(initialTasks);
     const [projects, setProjects] = useState(initialProjects);
-    const [sortConfig, setSortConfig] = useState({key: null, direction: "asc"});
+    const [sortConfig, setSortConfig] = useState({ key: null, direction: "asc" });
     const [currentTaskTitle, setCurrentTaskTitle] = useState("Task Title");
     const [editMode, isEditMode] = useState(false);
-    const [deletePopup, setDeletePopup] = useState({isOpen: false, taskId: null});
+    const [deletePopup, setDeletePopup] = useState({ isOpen: false, taskId: null });
+    const [activeTab, setActiveTab] = useState("upcoming");
+
+    const handleTabClick = (tab) => {
+        setActiveTab(tab);
+    };
+
 
     const getProjectName = (projectId) => {
         const project = projects.find(project => project.id === projectId);
@@ -56,7 +62,7 @@ export function TaskPage() {
 
 
     const handleDeletePopup = (action, taskId) => {
-        setDeletePopup({isOpen: false, taskId: null});
+        setDeletePopup({ isOpen: false, taskId: null });
         if (action === "delete") {
             deleteTask(taskId);
         }
@@ -204,7 +210,7 @@ export function TaskPage() {
         if (sortConfig.key === key && sortConfig.direction === "asc") {
             direction = "desc";
         }
-        setSortConfig({key, direction});
+        setSortConfig({ key, direction });
 
         const sortedTasks = [...tasks].sort((a, b) => {
             if (key === "priority") {
@@ -224,113 +230,137 @@ export function TaskPage() {
                 isOpen={deletePopup.isOpen}
             />
             <Sidebar
-                    id="title-option"
-                    title={currentTaskTitle}
-                    setTitleInParent={setCurrentTaskTitle}
-                    editMode={editMode}
-                    isEditMode={isEditMode}
+                id="title-option"
+                title={currentTaskTitle}
+                setTitleInParent={setCurrentTaskTitle}
+                editMode={editMode}
+                isEditMode={isEditMode}
+            >
+                <select
+                    id="projects-option"
+                    className="w-full p-2 border bg-white rounded focus:outline-none focus:ring-1 focus:ring-black mb-4"
                 >
-                    <select
-                        id="projects-option"
-                        className="w-full p-2 border bg-white rounded focus:outline-none focus:ring-1 focus:ring-black mb-4"
-                    >
-                        {projects.map((project) => (
-                            <option key={project.id} value={project.id} className="flex flex-col">
-                                {project.name}
-                            </option>
-                        ))}
-                    </select>
+                    {projects.map((project) => (
+                        <option key={project.id} value={project.id} className="flex flex-col">
+                            {project.name}
+                        </option>
+                    ))}
+                </select>
 
 
-                    <input
-                        id="date-option"
-                        type="date"
-                        className="flex h-10 w-full rounded-md border border-neutral-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300 mb-4"
-                    >
-                    </input>
+                <input
+                    id="date-option"
+                    type="date"
+                    className="flex h-10 w-full rounded-md border border-neutral-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300 mb-4"
+                >
+                </input>
 
 
-                    <select
-                        id="repeat-option"
-                        className="w-full p-2 border bg-white rounded focus:outline-none focus:ring-1 focus:ring-black mb-4"
-                    >
-                        <option value="Never">Repeats Never</option>
-                        <option value="option2">To be determined</option>
-                        <option value="option3">To be determined</option>
-                        <option value="option1">To be determined</option>
-                    </select>
+                <select
+                    id="repeat-option"
+                    className="w-full p-2 border bg-white rounded focus:outline-none focus:ring-1 focus:ring-black mb-4"
+                >
+                    <option value="Never">Repeats Never</option>
+                    <option value="option2">To be determined</option>
+                    <option value="option3">To be determined</option>
+                    <option value="option1">To be determined</option>
+                </select>
 
-                    <select
-                        id="priority-option"
-                        className="w-full p-2 border bg-white rounded focus:outline-none focus:ring-1 focus:ring-black mb-4"
-                    >
-                        <option value="0">Low</option>
-                        <option value="1">Medium</option>
-                        <option value="2">High</option>
-                        <option value="None">No Priority</option>
-                    </select>
+                <select
+                    id="priority-option"
+                    className="w-full p-2 border bg-white rounded focus:outline-none focus:ring-1 focus:ring-black mb-4"
+                >
+                    <option value="0">Low</option>
+                    <option value="1">Medium</option>
+                    <option value="2">High</option>
+                    <option value="None">No Priority</option>
+                </select>
 
 
-                    <Label htmlFor="descriptionBox"><b>Task Description</b></Label>
-                    <textarea
-                        id="descriptionBox"
-                        placeholder="Describe your task here..."
-                        spellCheck='false'
-                        style={{resize: 'none'}}
-                        className="flex w-full h-60 rounded-md border border-neutral-200 focus:outline-none focus:ring-1 focus:ring-black bg-white px-3 py-2 ring-offset-white file:border-0 file:bg-transparent file:font-light placeholder:text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 mb-4"
-                    >
-               </textarea>
-                    <div className="flex justify-end mb-4">
-                        <Button variant="default2" onClick={addNewTask}>Save Changes</Button>
-                    </div>
-                </Sidebar>
+                <Label htmlFor="descriptionBox"><b>Task Description</b></Label>
+                <textarea
+                    id="descriptionBox"
+                    placeholder="Describe your task here..."
+                    spellCheck='false'
+                    style={{ resize: 'none' }}
+                    className="flex w-full h-60 rounded-md border border-neutral-200 focus:outline-none focus:ring-1 focus:ring-black bg-white px-3 py-2 ring-offset-white file:border-0 file:bg-transparent file:font-light placeholder:text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 mb-4"
+                >
+                </textarea>
+                <div className="flex justify-end mb-4">
+                    <Button variant="default2" onClick={addNewTask}>Save Changes</Button>
+                </div>
+            </Sidebar>
 
             {/* Main Content */}
-            <div className="bg-white rounded-lg shadow mx-auto max-w-4xl p-6">
-                <h1 className="text-left pb-4 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                    My Tasks
-                </h1>
-                <br/>
-                <hr/>
-                <br/>
-                <div className="text-left mb-4">
-                    <Button onClick={resetTaskFields}>Create New Task</Button>
-                </div>
+            <div className="bg-white pt-5 pb-5 w-full">
+                <div className="flex flex-col gap-[32px]">
+                    <div className="flex flex-col gap-[32px]">
+                        <h1 className="text-left text-[48px] font-bold pl-20 pr-20">
+                            My Tasks
+                        </h1>
 
-                <table className="w-full">
-                    <thead>
-                    <tr className="border-b">
-                        <th className="px-4 py-2 text-center">Completed?</th>
-                        <th className="px-4 py-2 text-center" onClick={() => handleSort("title")}>Task <CaretSortIcon/>
-                        </th>
-                        <th className="px-4 py-2 text-center"
-                            onClick={() => handleSort("project")}>Project <CaretSortIcon/></th>
-                        <th className="px-4 py-2 text-center" onClick={() => handleSort("dueDate")}>Due
-                            Date <CaretSortIcon/></th>
-                        <th className="px-4 py-2 text-center"
-                            onClick={() => handleSort("priority")}>Priority <CaretSortIcon/></th>
-                        <th className="px-4 py-2"></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {tasks.map((task) => (
-                        <tr key={task.id} className="border-b last:border-b-0">
-                            <td className="px-4 py-2 text-center"><Checkbox id={`task-${task.id}`}
-                                                                            checked={task.completed}/></td>
-                            <td className="px-4 py-2 text-center">{task.title}</td>
-                            <td className="px-4 py-2 text-center">{getProjectName(task.project)}</td>
-                            <td className="px-4 py-2 text-center">{task.dueDate}</td>
-                            <td className="px-4 py-2 text-center">{task.priority}</td>
-                            <td className="px-4 py-2 text-center">
-                                <Button variant="ghost" size="icon"
-                                        onClick={() => setDeletePopup({isOpen: true, taskId: task.id})}>
-                                    <Trash2 className="h-4 w-4"/>
-                                </Button>
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
+                        {/* Tab Buttons for Upcoming and Completed */}
+                        <div className="border-b">
+                            <div className="flex pl-20 pr-20 gap-[32px]">
+                            <button
+                                className={`text-[16px] font-semibold ${activeTab === 'upcoming' ? 'text-black border-b-2 border-black' : 'text-gray-500'}`}
+                                onClick={() => handleTabClick("upcoming")}
+                            >
+                                Upcoming
+                            </button>
+                            <button
+                                className={`text-[16px] font-semibold ${activeTab === 'completed' ? 'text-black border-b-2 border-black' : 'text-gray-500'}`}
+                                onClick={() => handleTabClick("completed")}
+                            >
+                                Completed
+                            </button>
+                            </div>
+                        </div>
+
+                        <div className="text-left pl-20 pr-20">
+                            <Button onClick={resetTaskFields}>Create New Task</Button>
+                        </div>
+                    </div>
+
+
+                    <table className="w-full">
+                        <thead>
+                            <tr className="border-b">
+                                <th className="px-4 py-2 text-center">Completed?</th>
+                                <th className="px-4 py-2 text-center" onClick={() => handleSort("title")}>Task <CaretSortIcon />
+                                </th>
+                                <th className="px-4 py-2 text-center"
+                                    onClick={() => handleSort("project")}>Project <CaretSortIcon /></th>
+                                <th className="px-4 py-2 text-center" onClick={() => handleSort("dueDate")}>Due
+                                    Date <CaretSortIcon /></th>
+                                <th className="px-4 py-2 text-center"
+                                    onClick={() => handleSort("priority")}>Priority <CaretSortIcon /></th>
+                                <th className="px-4 py-2"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {tasks
+                                .filter(task => (activeTab === "upcoming" ? !task.completed : task.completed))
+                                .map((task) => (
+                                    <tr key={task.id} className="border-b last:border-b-0">
+                                        <td className="px-4 py-2 text-center"><Checkbox id={`task-${task.id}`}
+                                            checked={task.completed} /></td>
+                                        <td className="px-4 py-2 text-center">{task.title}</td>
+                                        <td className="px-4 py-2 text-center">{getProjectName(task.project)}</td>
+                                        <td className="px-4 py-2 text-center">{task.dueDate}</td>
+                                        <td className="px-4 py-2 text-center">{task.priority}</td>
+                                        <td className="px-4 py-2 text-center">
+                                            <Button variant="ghost" size="icon"
+                                                onClick={() => setDeletePopup({ isOpen: true, taskId: task.id })}>
+                                                <Trash2 className="h-4 w-4" />
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </table>
+
+                </div>
             </div>
         </>
     );
