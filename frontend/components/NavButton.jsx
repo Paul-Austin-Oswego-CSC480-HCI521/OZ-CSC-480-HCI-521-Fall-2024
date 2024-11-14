6a68d2a85103634e9d17255caaa6ab6ae16005ff
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-11-14 17:23:46 -0500

    Fix navigation positioning

--- frontend/components/NavButton.jsx
+++ frontend/components/NavButton.jsx
@@ -1,8 +1,8 @@
 // Custom nav link button component for left-side bar
 import { NavLink } from "react-router-dom"
 // Keep spaces on either side of class lists!
-const activeClasses = " border border-neutral-200 bg-white hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 "
-const inactiveClasses = " border border-neutral-300 bg-[#CBD5E1] hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 "
+const activeClasses = "  bg-white hover:bg-neutral-100 hover:text-neutral-900"
+const inactiveClasses = " bg-blueLight hover:bg-neutral-100 hover:text-neutral-900"
 const baseClasses = " py-2 pl-3  mx-4 inline-flex items-center justify-left whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300 "
 function NavButton(props){
     return (
--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -11,18 +11,18 @@ function PageLayout() {
   return (
       <div>
           <header className="sticky z-50 left-0 right-0 top-0">
-             <div className="flex items-center bg-blueHighlight">
+             <div className="flex items-center bg-blueHighlight relative z-10">
                   <a href="/" title='Home' className="flex items-center gap-3 py-4 pl-4">
                     <img className="p-0.5" width={32} height={32} src={logoWhite} alt=""></img>
                     <h1 className="text-2xl text-white font-bold">CheckMate</h1>
                   </a>
               </div>
+              <SidePanel/>              
           </header>
-          <main className='flex justify-between '>
-          <SidePanel/>
-          <Drawer direction={"right"}>
-            <Outlet />
-          </Drawer>
+          <main className='ml-[222px]'>
+            <Drawer direction={"right"}>
+                <Outlet />
+            </Drawer>
         </main>
       </div>
 
