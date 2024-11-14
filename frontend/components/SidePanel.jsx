ffdf8b3c409c7a7cae0dc05168d96ce408bdc6d6
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-11-14 18:10:07 -0500

    Fix logout button position, html semantics

--- frontend/components/SidePanel.jsx
+++ frontend/components/SidePanel.jsx
@@ -5,35 +5,29 @@ import {LogOutIcon} from "lucide-react"
 
 export function SidePanel() {
     return (
-        <header className=" bg-blueLight w-[222px] fixed top-0 bottom-0 pt-16">
-            <nav className="mt-6">
-                <ul className="flex flex-col">
-                    <li className="flex flex-col">
-                        <NavButton href="/">My Tasks</NavButton>
-                    </li>
-                    <li className="flex flex-col">
-                        <NavButton href="/deleted">Recently Deleted</NavButton>
-                    </li>
-                    <li className="flex flex-col">
-                        <NavButton href="/shared">Shared with Me</NavButton>
-                    </li>
-                </ul>
-            </nav>
-            <hr className="w-48 mx-auto my-6 border-neutral-400"/>
-            <nav className="flex flex-col items-center">
-                <ProjectAccordion/>
-            </nav>
-            <nav className="">
-                <ul className="flex flex-col ">
-                    <li className="flex flex-col fixed bottom-10 min-w-56 ">
-                        <NavButton href="/Login">Log out
-                            <LogOutIcon className="resize scale-50 translate-x-24"></LogOutIcon>
-                        </NavButton>
-
-                    </li>
-                </ul>
-            </nav>
-
-        </header>
+        <nav className="mt-6 bg-blueLight w-[222px] fixed top-0 bottom-0 pt-16">
+            <ul className="flex flex-col h-full">
+                <li className="flex flex-col">
+                    <NavButton href="/">My Tasks</NavButton>
+                </li>
+                <li className="flex flex-col">
+                    <NavButton href="/deleted">Recently Deleted</NavButton>
+                </li>
+                <li className="flex flex-col">
+                    <NavButton href="/shared">Shared with Me</NavButton>
+                </li>
+                <hr className="w-48 mx-auto my-6 border-neutral-400"/>
+                <li>
+                    <menu className="flex flex-col items-center">
+                        <ProjectAccordion/>
+                    </menu>
+                </li>
+                <li className="flex flex-col mb-8 mt-auto ">
+                    <NavButton href="/Login">Log out
+                        <LogOutIcon className="resize scale-50 translate-x-24"></LogOutIcon>
+                    </NavButton>
+                </li>
+            </ul>
+        </nav>
     )
 }
