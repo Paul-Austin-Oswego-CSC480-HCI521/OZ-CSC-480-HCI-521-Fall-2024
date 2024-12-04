cd21cd2834b822537b9cc27c7aecd62afca24720
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-12-03 23:42:33 -0500

    Update navigation styling

--- frontend/components/ProjectAccordion.jsx
+++ frontend/components/ProjectAccordion.jsx
@@ -7,8 +7,7 @@ import {
 } from "@/components/ui/accordion";
 import {Button} from "./ui/button";
 import NavButton from "./NavButton";
-import {PlusIcon, TrashIcon} from "@radix-ui/react-icons";
-import {Trash2Icon} from "lucide-react";
+import {TrashIcon, ChevronDownIcon} from "@radix-ui/react-icons";
 
 function ProjectAccordion() {
     const [projects, setProjects] = useState([]);
@@ -70,44 +69,50 @@ function ProjectAccordion() {
         <Accordion defaultValue="projects" type="single" collapsible>
             <AccordionItem value="projects"
                            className="bg-none">
-                <div className="relative flex">
+                <div className="relative">
+                    <AccordionTrigger className="hover:no-underline py-3 w-[12rem]">
+                        <span className="pl-3 font-normal">My Projects</span>
+                        <ChevronDownIcon className="text-gray-500 w-5 h-5 mr-3 fill-black"/>
+                    </AccordionTrigger>
+                    {/* added onclick to button */}
                     <Button
                         size="icon"
                         variant="ghost"
+                        onClick={addProject}
                         //added 'flex justify-center items-center'
-                        className="absolute z-10 w-8 h-8 top-1.5 hover:border hover:border-neutral-200 hover:bg-neutral-50 right-10 flex justify-center items-center"
+                        className="absolute z-10 w-10 h-10 top-1  left-28 hover:border hover:border-neutral-200 hover:bg-neutral-50"
                     >
-                        {/* added onclick to svg */}
+                        
                         <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"
-                             onClick={addProject}>
+                             >
                             {/* replaced <addProjectpath with <path*/}
                             <path
                                 d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z"
                                 fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
                         </svg>
                     </Button>
-                    <AccordionTrigger className="hover:no-underline py-3 px-2 w-44">
-                        <span className="pl-2">My Projects</span>
-                    </AccordionTrigger>
                 </div>
                 <AccordionContent className="">
-                    <ul className="flex flex-col gap-1 -mx-2 -mb-2">
+                    <ul className="flex flex-col justify-items-start gap-1">
                         {projects.map((project) => (
-                            <li key={project.id} className="relative grid grid-cols-[99%_1%] gap-2 items-center">
-                                {/* Project Name */}
-                                <NavButton href={`/project/${project.id}`}
-                                           className="flex items-center justify-between w-full">
-        <span className="flex items-center justify-between w-full pr-4">
-            <span>{project.name}</span>
-        </span>
-                                </NavButton>
-
-                                {/* Trash Icon with Absolute Position */}
-                                <button
-                                    onClick={() => deleteProject(project.id)}
-                                    className="absolute right-5 p-1 rounded-full hover:bg-gray-200 flex justify-center items-center">
-                                    <TrashIcon className="text-gray-500"/>
-                                </button>
+                            <li key={project.id}>
+                                <ul className="relative">
+                                    <li>
+                                        <NavButton href={`/project/${project.id}`}
+                                            className="flex items-center justify-between">
+                                            <span className="w-[11rem]">
+                                                {project.name}
+                                            </span>
+                                        </NavButton>
+                                    </li>
+                                    <li className="absolute right-0 top-0 mr-1">
+                                        <button
+                                            onClick={() => deleteProject(project.id)}
+                                            className="w-9 h-9 rounded-md flex items-center justify-center hover:bg-gray-200">
+                                            <TrashIcon className="text-gray-500 w-5 h-5"/>
+                                        </button>
+                                    </li>
+                                </ul>
                             </li>
 
                         ))}
