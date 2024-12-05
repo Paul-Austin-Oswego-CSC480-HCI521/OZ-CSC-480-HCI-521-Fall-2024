4e83df9782fb13ebc023867133df8c0490cba109
piesarentsquare <jjhh12@outlook.com>
2024-12-05 17:58:46 -0500

    decouple task pages to make them much more usable

--- frontend/src/pages/ProjectPage.jsx
+++ frontend/src/pages/ProjectPage.jsx
@@ -1,9 +1,56 @@
-import { useParams } from "react-router-dom";
-import { TaskPage } from "./TaskPage";
+import { useParams } from 'react-router-dom'
+import { TaskPageContent, TaskPageHeaders } from './TaskPage'
+import { useContext, useEffect, useState } from 'react'
+import { fetchTasks, formatTasks, ProjectContext } from '@/lib/taskProjectUtils'
+import PageTitle from '@/components/PageTitle'
+import { TaskSidePanel } from '@/components/TaskSidePanel'
+import { TaskTable } from '@/components/TaskTable'
+import { taskColumnsProject } from '@/components/TaskColumnsProject'
 
 export default function ProjectPage() {
-    let {projectID} = useParams();
+    const { projectID } = useParams()
+    const { projects } = useContext(ProjectContext)
+    const [activeTab, setActiveTab] = useState('upcoming')
+    const [tasks, setTasks] = useState({})
+    const [pageTitle, setPageTitle] = useState(`Project ${projectID}`)
+    const [selectedProject, setSelectedProject] = useState(null)
+    const [lastSelectedTask, setLastSelectedTask] = useState(null)
+
+    useEffect(() => {
+        if (projects == null || projects.length === 0)
+            return
+
+        const p = projects.find(project => project.id === +projectID)
+        if (p) {
+            setPageTitle(p.name)
+            setSelectedProject(p)
+        } else
+            console.error('TODO: replace with 404')
+        
+        fetchTasks(setTasks)
+    }, [projects, projectID])
+
     return (
-        <TaskPage variant={'project'} projectId={+projectID} />
+        <>
+            <PageTitle title={`${pageTitle} | Checkmate`}/>
+            <TaskSidePanel
+                selectedTask={lastSelectedTask}
+                setTasks={setTasks}
+                projects={projects}
+                selectedProject={selectedProject}
+            />
+            <TaskPageContent pageTitle={pageTitle} headers={
+                <TaskPageHeaders activeTab={activeTab} setActiveTab={setActiveTab} onCreateTask={() => setLastSelectedTask(null)}/>
+            }>
+                <TaskTable
+                    columns={taskColumnsProject}
+                    data={formatTasks(tasks, projects, activeTab, selectedProject)}
+                    onTaskSelect={taskId => setLastSelectedTask(tasks[taskId])}
+                    selectedTask={lastSelectedTask}
+                    tasks={tasks}
+                    setTasks={setTasks}
+                />
+            </TaskPageContent>
+        </>
     )
 }
--- frontend/src/pages/TaskPage.jsx
+++ frontend/src/pages/TaskPage.jsx
@@ -1,44 +1,23 @@
-import React, { useContext, useEffect, useMemo, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { fetchTasks, formatTasks, ProjectContext } from '@/lib/taskProjectUtils'
 
 import { Button } from '@/components/ui/button'
 import { TaskTable } from '@/components/TaskTable'
 import PageTitle from '@/components/PageTitle'
 import { TaskSidePanel } from '@/components/TaskSidePanel'
-import { taskColumnsProject } from '@/components/TaskColumnsProject'
-import { taskColumnsDeleted } from '@/components/TaskColumnsDeleted'
 import { taskColumns } from '@/components/TaskColumns'
 
 import { DrawerTrigger, DrawerContent, DrawerTitle, DrawerHeader } from '@/components/ui/drawer'
 import { Drawer, Drawer as DrawerPrimative } from 'vaul'
 import { faL } from '@fortawesome/free-solid-svg-icons'
 
-export const TaskPage = ({variant, projectId}) => {
-    const isProject = useMemo(() => variant === 'project', [variant])
-    const [pageTitle, setPageTitle] = useState(isProject ? 'Project' : 'My Tasks')
+export const TaskPage = () => {
     const [tasks, setTasks] = useState({})
     const { projects } = useContext(ProjectContext)
     const [selectedProject, setSelectedProject] = useState(null)
     const [activeTab, setActiveTab] = useState("upcoming")
     const [lastSelectedTask, setLastSelectedTask] = useState(null)
     const [isDrawerOpen, setIsDrawerOpen] = useState(false)
-
-    const isTrash = useMemo(() => variant === 'trash', [variant])
-    const columns = useMemo(() => {
-        if (isProject)
-            return taskColumnsProject
-        else if (isTrash)
-            return taskColumnsDeleted
-        else
-            return taskColumns
-    }, [isProject, isTrash])
-    const tableTasks = useMemo(() => {
-        const formatted = formatTasks(tasks, projects, isProject ? selectedProject : null)
-        if (isTrash)
-            return formatted
-        else
-            return formatted.filter(task => (activeTab === "upcoming") ? !task.completed : task.completed)
-    }, [isProject, isTrash, projectId, tasks, activeTab])
     
     const handleTaskSelect = (taskId) => {
         setIsDrawerOpen(true);
@@ -57,24 +36,11 @@ export const TaskPage = ({variant, projectId}) => {
     useEffect(() => {
         if (projects == null || projects.length === 0)
             return
-        
-        if (isProject) {
-            const p = projects.find(project => project.id === projectId)
-            if (p) {
-                setPageTitle(p.name)
-                setSelectedProject(p)
-            }
-            else
-                console.error('TODO: replace this with a 404 page')
-        } else if (!selectedProject || projects.find(p => selectedProject.id === p.id) == undefined)
+        if (!selectedProject || projects.find(p => selectedProject.id === p.id) == undefined)
             setSelectedProject(projects[0])
 
-        if (isTrash)
-            setPageTitle('Recently Deleted')
-
-
-        fetchTasks(setTasks, isTrash)
-    }, [projects, projectId])
+        fetchTasks(setTasks)
+    }, [projects])
 
     const resetTaskFields = () => {
         setIsDrawerOpen(true);
@@ -83,7 +49,7 @@ export const TaskPage = ({variant, projectId}) => {
 
     return (
         <>
-            <PageTitle title={`${pageTitle} | Checkmate`}></PageTitle>
+            <PageTitle title={`My Tasks | Checkmate`}></PageTitle>
 
             {/* <DrawerContent className={`max-w-[417px] bg-blueLight fixed bottom-0 right-0 ml-auto h-full pt-20 pl-4 ${isDrawerOpen ? 'block' : 'hidden'}`}> */}
                 {/* <DrawerHeader>
@@ -92,60 +58,70 @@ export const TaskPage = ({variant, projectId}) => {
 
 
                 {/* A.D. - Commented out the Drawer changes because it was causing issues w/ selecting tasks*/}
-                {!isTrash ? 
-                    <TaskSidePanel selectedTask={lastSelectedTask} setTasks={setTasks} projects={projects} selectedProject={selectedProject} />
-                : <></>}
+                
+                <TaskSidePanel
+                    selectedTask={lastSelectedTask}
+                    setTasks={setTasks}
+                    projects={projects}
+                    selectedProject={selectedProject}
+                />
+                
             {/* </DrawerContent> */}
 
-
-
-
             {/* Main Content */}
-            <div className="bg-white pt-5 pb-5 w-full">
-                <div className="flex flex-col gap-[32px]">
-                    <div className="flex flex-col gap-[32px]">
-                        <h1 className="text-left text-[48px] font-bold pl-20 pr-20">
-                            {pageTitle}
-                        </h1>
-
-                        {!isTrash ?
-                            <>
-                                {/* Tab Buttons for Upcoming and Completed */}
-                                <div className="border-b">
-                                    <div className="flex pl-20 pr-20 gap-[32px]">
-                                        <button
-                                            className={`text-[16px] font-semibold ${activeTab === 'upcoming' ? 'text-black border-b-2 border-black' : 'text-gray-500'}`}
-                                            onClick={() => setActiveTab("upcoming")}
-                                        >
-                                            Upcoming
-                                        </button>
-                                        <button
-                                            className={`text-[16px] font-semibold ${activeTab === 'completed' ? 'text-black border-b-2 border-black' : 'text-gray-500'}`}
-                                            onClick={() => setActiveTab("completed")}
-                                            >
-                                            Completed
-                                        </button>
-                                    </div>
-                                </div>
-                                <DrawerTrigger asChild>
-                                    <div className="text-left pl-20 pr-20">
-                                        <Button onClick={resetTaskFields}>Create New Task</Button>
-                                    </div>
-                                </DrawerTrigger>
-                            </>
-                        : <></> }
-                    </div>
-                    <section>
-                        <TaskTable columns={columns}
-                                   data={tableTasks}
-                                   onTaskSelect={handleTaskSelect}
-                                   selectedTask={lastSelectedTask}
-                                   tasks={tasks}
-                                   setTasks={setTasks}
-                                   />
-                    </section>
-                </div>
-            </div>
+            <TaskPageContent pageTitle={'My Tasks'} headers={
+                <TaskPageHeaders activeTab={activeTab} setActiveTab={setActiveTab} onCreateTask={resetTaskFields}/>
+            }>
+                <TaskTable
+                    columns={taskColumns}
+                    data={formatTasks(tasks, projects, activeTab)}
+                    onTaskSelect={handleTaskSelect}
+                    selectedTask={lastSelectedTask}
+                    tasks={tasks}
+                    setTasks={setTasks}
+                />
+            </TaskPageContent>
         </>
     )
 }
+
+export const TaskPageContent = ({pageTitle, headers, children}) => (
+    <div className="bg-white pt-5 pb-5 w-full">
+        <div className="flex flex-col gap-[32px]">
+            <div className="flex flex-col gap-[32px]">
+                <h1 className="text-left text-[48px] font-bold pl-20 pr-20">
+                    {pageTitle}
+                </h1>
+                {headers}
+            </div>
+            <section>
+                {children}
+            </section>
+        </div>
+    </div>
+)
+
+export const TaskPageHeaders = ({activeTab, setActiveTab, onCreateTask}) => (<>
+    {/* Tab Buttons for Upcoming and Completed */}
+    <div className="border-b">
+    <div className="flex pl-20 pr-20 gap-[32px]">
+        <button
+            className={`text-[16px] font-semibold ${activeTab === 'upcoming' ? 'text-black border-b-2 border-black' : 'text-gray-500'}`}
+            onClick={() => setActiveTab('upcoming')}
+        >
+            Upcoming
+        </button>
+        <button
+            className={`text-[16px] font-semibold ${activeTab === 'completed' ? 'text-black border-b-2 border-black' : 'text-gray-500'}`}
+            onClick={() => setActiveTab('completed')}
+            >
+            Completed
+        </button>
+    </div>
+    </div>
+    <DrawerTrigger asChild>
+    <div className="text-left pl-20 pr-20">
+        <Button onClick={onCreateTask}>Create New Task</Button>
+    </div>
+    </DrawerTrigger>
+</>)
